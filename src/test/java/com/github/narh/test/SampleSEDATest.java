c98fc198386e3f91e57c0aa9b239a683db7dc9bd
成田　英樹 <naritaenator@gmail.com>
2019-11-13 20:10:47 +0900

    SEDA キューのテスト追加

---
+++ src/test/java/com/github/narh/test/SampleSEDATest.java
@@ -0,0 +1,41 @@
+package com.github.narh.test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ServiceStatus;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class SampleSEDATest {
+
+  Logger log = LoggerFactory.getLogger(getClass());
+
+  @Autowired
+  private CamelContext camelContext;
+
+  @EndpointInject(uri = "mock:result")
+  private MockEndpoint resultEndpoint;
+
+  @Test
+  public void キューのテスト() throws Exception {
+    assertThat("ステータス", camelContext.getStatus(), is(ServiceStatus.Started));
+    resultEndpoint.expectedMessageCount(1000);
+    resultEndpoint.assertIsSatisfied();
+
+    log.info("=== END ===");
+  }
+}
