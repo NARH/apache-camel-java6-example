f3a29c651a64a6518303c7341a35db8440a49897
成田　英樹 <naritaenator@gmail.com>
2019-11-19 23:57:55 +0900

    ファイルの暗号化移行のテスト

--- src/main/java/com/github/narh/tool/crypt/CryptDataFormat.java
+++ src/main/java/com/github/narh/tool/crypt/CryptDataFormat.java
@@ -7,6 +7,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.spi.DataFormat;
 import org.apache.camel.util.ExchangeHelper;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,14 @@ public class CryptDataFormat implements DataFormat {
   private Crypter srcCrypter;
   private Crypter destCrypter;
 
-  @Autowired @Qualifier("srcCrypter")
+  @Autowired
+  @Qualifier("srcCrypter")
   public void setSrcCrypter(Crypter srcCrypter) {
     this.srcCrypter = srcCrypter;
   }
 
-  @Autowired @Qualifier("destCrypter")
+  @Autowired
+  @Qualifier("destCrypter")
   public void setDestCrypter(Crypter destCrypter) {
     this.destCrypter = destCrypter;
   }
@@ -31,15 +34,22 @@ public class CryptDataFormat implements DataFormat {
   @Override
   public void marshal(Exchange exchange, Object graph, OutputStream stream) throws Exception {
     String text = ExchangeHelper.convertToType(exchange, String.class, graph);
-    if(log.isDebugEnabled()) {
-      log.debug("AES128で暗号 {} -> {}", text,
-          new String(Hex.encodeHex(srcCrypter.encrypt(text.getBytes("MS932")))));
+
+    if (StringUtils.isNotEmpty(text)) {
+      if (log.isDebugEnabled()) {
+        log.debug("AES128で暗号 {} -> {}", text,
+            new String(Hex.encodeHex(srcCrypter.encrypt(text.getBytes("MS932")))));
+      }
+      String decryptStr = new String(srcCrypter.decrypt(Hex.decodeHex(text)));
+      if (log.isDebugEnabled()) log.debug("複合 {} -> {}", text, decryptStr);
+      String encryptStr = new String(Hex.encodeHex(destCrypter.encrypt(decryptStr.getBytes("MS932"))));
+      if (log.isDebugEnabled()) log.debug("暗号 {} -> {}", decryptStr, encryptStr);
+      if (StringUtils.isNotEmpty(encryptStr)) {
+        stream.write(encryptStr.getBytes("MS932"));
+        if(null == exchange.getProperty("CamelSplitComplete")
+            || !exchange.getProperty("CamelSplitComplete", Boolean.class)) stream.write("\r\n".getBytes("MS932"));
+      }
     }
-    String decryptStr = new String(srcCrypter.decrypt(Hex.decodeHex(text)));
-    if(log.isDebugEnabled()) log.debug("複合 {} -> {}", text, decryptStr);
-    String encryptStr = new String(Hex.encodeHex(destCrypter.encrypt(decryptStr.getBytes("MS932"))));
-    if(log.isDebugEnabled()) log.debug("暗号 {} -> {}", decryptStr, encryptStr);
-    stream.write(encryptStr.getBytes("MS932"));
   }
 
   @Override
